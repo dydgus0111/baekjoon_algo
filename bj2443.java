9d29a0d361e2d2a238e9681b536cdb66ebaec91d
KimYongHyeon <kkskww@gmail.com>
2019-02-20 14:48:47 +0000

    Update bj2443.java

---
+++ bj2443.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class bj2443 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=n;i>0;i--) {
+			for(int k=0;k<n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
